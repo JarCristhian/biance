c6e0bd297deec95199f88224ce00b0a150e3d46b
jarcristhian <jarcy.dev@gmail.com>
2026-01-10 00:15:55 -0500

    changes v1.3

--- src/app/graphics/page.tsx
+++ src/app/graphics/page.tsx
@@ -130,7 +130,7 @@ export default function GraphicsPage() {
         </div>
       </header>
 
-      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-3 md:mt-0 overflow-y-auto h-[calc(100dvh-8rem)]">
+      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-3 md:mt-0 overflow-y-auto h-[calc(100dvh-10rem)]">
         <AnimatePresence mode="wait">
           <motion.div
             key={activeTab}
